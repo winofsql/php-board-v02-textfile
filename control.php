0e30261ef0b4cb9421f5320989a44a00702e4dc5
sworc <lightbox@nifty.com>
2024-09-22 16:11:02 +0900

    Update control.php

--- control.php
+++ control.php
@@ -6,22 +6,31 @@ header( "Content-Type: text/html; charset=utf-8" );
 require_once("model.php");
 
 $dataFile = 'bbs.dat';
+// 表示データ
 $line_data = "";
+// 投稿件数(表示用)
 $kensu = "";
 
+// MARK:送信処理
 // POST ( FORM からの送信 )
 if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
 
+    // 送信時はまず書き込み
+    // ( この後で表示されます )
     write_data();
 
 }
 
 // *************************************
+// MARK:読み込み
 // ファイルの読込み( A )
 // テキストファイルを読み込んで行を要素とした配列にする
+// ▼ FILE_IGNORE_NEW_LINES
+// 配列の各要素の最後の改行を省略します。
 // *************************************
 $posts = @file($dataFile, FILE_IGNORE_NEW_LINES);
 
+// MARK:逆ソート
 // 配列を逆順に変換する( 最後に追加した行が先頭になる )
 if ( $posts !== FALSE ) {
     $posts = array_reverse($posts);
@@ -31,9 +40,15 @@ else {
     $posts = array();
 }
 
+// MARK:投稿件数
 $kensu = count($posts);
 
+
+
+
+
 // *************************************
+// MARK:データ作成
 // 画面用データの作成( B )
 // *************************************
 if ( $kensu != 0 ) {
@@ -53,7 +68,7 @@ else {
 
 
 // *************************************
-// 画面
+// MARK:画面
 // *************************************
 require_once("view.php");
 
